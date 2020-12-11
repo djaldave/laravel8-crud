206ab9a040c29567ccf920cf0e2249782cee661c
djaldave <djaldave@gmail.com>
2020-12-11 11:33:13 +0800

    routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource("index", "App\Http\Controllers\CrudController");
+Route::resource("/", "App\Http\Controllers\CrudController");
+
